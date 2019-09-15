4403734c3bb7356d58307fd7969936a2a739fad9
Juan Ventura <jmvj03@outlook.com>
2019-09-14 22:14:11 -0500

    Actividad de clase

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 2020-1 on 8/9/19.
+//  Copyright © 2019 unam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func
+        unwindToPortada(unwindSege: UIStoryboardSegue){
+        //Accion disponible en el proyecto
+    }
+
+
+}
+
